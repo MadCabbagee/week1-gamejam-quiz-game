f6eb4db7cb45d42022346d39dc1159e1ea016271
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-07-26 00:57:23 -0600

    create main class

---
+++ src/main/java/codejam/quizgame/QuizGame.java
@@ -0,0 +1,12 @@
+package codejam.quizgame;
+
+public class QuizGame {
+
+    public static void main(String[] args) {
+        new QuizGame().start();
+    }
+
+    private void start() {
+
+    }
+}
