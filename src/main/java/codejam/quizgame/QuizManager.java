426ea3c9e9c31b1fbca532fa0c54667da9e806b5
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-07-27 17:22:14 -0600

    added some more methods, implemented printQuizes

--- src/main/java/codejam/quizgame/QuizManager.java
+++ src/main/java/codejam/quizgame/QuizManager.java
@@ -1,9 +1,18 @@
 package codejam.quizgame;
 
+import java.util.Scanner;
+
 public class QuizManager {
     private Quiz[] quizes;
 
+    public QuizManager() {
+        /*this.quizes = new Quiz[2];
+        quizes[0] = new Quiz("test1", new Question[5]);
+        quizes[1] = new Quiz("test2", new Question[5]);*/
+    }
+
     public void createQuiz() {
+        Scanner cin = new Scanner(System.in);
         // ask user stuff for creating quiz then create it and add to list
     }
 
@@ -11,8 +20,31 @@ public class QuizManager {
         // ask suer stuff for editing quiz then edit it
     }
 
+    public void deleteQuiz() {
+
+    }
+
+    public void playQuiz() {
+        // show quizes and ask to choose one
+        printQuizes();
+
+        // user select quiz
+        System.out.println("Select a quiz to play.");
+
+
+        // quiz start running
+        // show score at end or keep running score (correctQ/totalQ)
+    }
+
     public void printQuizes() {
         // print all quizes names from the list
+        StringBuilder sb = new StringBuilder();
+        sb.append("All Quizes:\n");
+        for (int i = 0; i < quizes.length; i++) {
+            Quiz q = quizes[i];
+            sb.append(String.format("\t%d. %s\n", i+1, q.getName()));
+        }
+        System.out.println(sb);
     }
 
     public void loadQuizes() {
