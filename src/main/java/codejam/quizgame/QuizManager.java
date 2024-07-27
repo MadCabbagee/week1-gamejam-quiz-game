e24d8aff957b6b396d10ded68fc3d64991f0e53f
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-07-26 23:20:59 -0600

    stub basics of the class

---
+++ src/main/java/codejam/quizgame/QuizManager.java
@@ -0,0 +1,25 @@
+package codejam.quizgame;
+
+public class QuizManager {
+    private Quiz[] quizes;
+
+    public void createQuiz() {
+        // ask user stuff for creating quiz then create it and add to list
+    }
+
+    public void editQuiz() {
+        // ask suer stuff for editing quiz then edit it
+    }
+
+    public void printQuizes() {
+        // print all quizes names from the list
+    }
+
+    public void loadQuizes() {
+        // load quizes from file
+    }
+
+    public void saveQuizes() {
+        // save created quizes from memory to file
+    }
+}
