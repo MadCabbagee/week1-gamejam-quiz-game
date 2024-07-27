ee4df77d4a86ceccefc3d3cb077a809923dd0907
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-07-27 12:18:02 -0700

    Added variables, didn't implement methods

--- src/main/java/codejam/quizgame/Quiz.java
+++ src/main/java/codejam/quizgame/Quiz.java
@@ -1,4 +1,22 @@
 package codejam.quizgame;
 
 public class Quiz {
+
+//    Quiz:
+//    name: String
+//    questions: codejam.quizgame.Question[]
+//
+//    play(): void - randomize a local copy of the questions
+//    print(): void
+
+    String name;
+    Question[] questions;
+
+    void play() {
+
+    }
+
+    void print() {
+        // Im not sure how to link the quiz with it's appropriate questions/answers
+    }
 }
