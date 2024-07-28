6b13c50f43aa80e0fd4aa49c075f315266046967
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-07-27 22:50:45 -0600

    refactor and make small changes

--- src/main/java/codejam/quizgame/Menu.java
+++ src/main/java/codejam/quizgame/Menu.java
@@ -6,10 +6,13 @@ import java.util.Scanner;
 
 public class Menu {
 
+    private final String name;
     private final String[] choices;
     private final Map<Integer, Runnable> selectionToActions;
+    private boolean run;
 
-    public Menu(String[] choices, Runnable[] actions) {
+    public Menu(String name, String[] choices, Runnable[] actions) {
+        this.name = name;
         this.choices = choices;
         this.selectionToActions = new HashMap<>();
         for (int i = 0; i < actions.length; i++) {
@@ -17,30 +20,24 @@ public class Menu {
         }
     }
 
-    public void handleUserInput() {
-        Scanner consoleIn = new Scanner(System.in);
+    public void handleUserSelection() {
+        run = true;
+        while (run) {
+            int choice = Console.promptWithChoicesSpaced(name + ':', choices);
 
-        display();
-        System.out.print("Enter your choice: ");
-        while (!consoleIn.hasNextInt()) {
-            System.out.println("Invalid input. Please enter a number.");
-            if (consoleIn.hasNext()) {
-                consoleIn.next(); // i think this is here to flush extra stuff in the scanner, donno will see if its even needed
-            }
-        }
-        int choice = consoleIn.nextInt();
-        if (choice > 0 && choice < choices.length) {
             Runnable action = selectionToActions.get(choice);
             action.run();
-        } else {
-            System.out.println("Invalid selection. Please enter one of the numbers from the menu.");
         }
-        consoleIn.close();
+
     }
 
     private void display() {
         for (int i = 0; i < choices.length; i++) {
             System.out.println(i+1 + ". " + choices[i]);
         }
+    }
+
+    public void stop() {
+        run = false;
     }
 }
