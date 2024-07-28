77a97f030b2d6c7965548e9cfb2f0f14ace3e9f3
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-07-27 22:52:58 -0600

    continue implementation, refactoring, and small changes.

--- src/main/java/codejam/quizgame/QuizMaker.java
+++ src/main/java/codejam/quizgame/QuizMaker.java
@@ -1,60 +1,52 @@
 package codejam.quizgame;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class QuizMaker {
-
-    private boolean run;
-    private static BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+    private final QuizManager qMan;
     private final Menu mainMenu;
     private final Menu playMenu;
     private final Menu quizMenu;
-    private final QuizManager qMan;
+
 
     public QuizMaker() {
         qMan = new QuizManager();
-        final Runnable returnToMainMenu = () -> {/* will return to main loop which will prompt with main menu again */};
 
         String[] mainChoices = new String[] {"Play", "Manage Quizes", "Exit"};
-        Runnable[] mainActions = new Runnable[] {this::showPlayMenu, this::showQuizMenu, () -> run = false};
-        mainMenu = new Menu(mainChoices, mainActions);
+        Runnable[] mainActions = new Runnable[] {this::showPlayMenu, this::showQuizMenu, this::exit};
+        mainMenu = new Menu("Main Menu", mainChoices, mainActions);
 
         String[] playChoices = new String[] {"New", "Continue", "Go Back"};
-        Runnable[] playActions = {this::playNewQuiz, this::continueActiveQuiz, returnToMainMenu };
-        playMenu = new Menu(playChoices, playActions);
+        Runnable[] playActions = {qMan::playQuiz, qMan::continueActiveQuiz, this::closePlayMenu };
+        playMenu = new Menu("Play Menu", playChoices, playActions);
 
         String[] quizChoices = new String[] {"New", "Edit", "Delete", "Go Back"};
-        Runnable[] quizActions = new Runnable[] {qMan::createQuiz, qMan::editQuiz, qMan::deleteQuiz, returnToMainMenu};
-        quizMenu = new Menu(quizChoices, quizActions);
+        Runnable[] quizActions = new Runnable[] {qMan::createQuiz, qMan::editQuiz, qMan::deleteQuiz, this::closeQuizMenu};
+        quizMenu = new Menu("Quiz Menu", quizChoices, quizActions);
     }
 
-    private void continueActiveQuiz() {
-        // check active exists. if not tell them
-        // else continue active quiz loaded from file (serialized java object)
+    private void closeQuizMenu() {
+        quizMenu.stop();
     }
 
-    private void playNewQuiz() {
+    private void closePlayMenu() {
+        playMenu.stop();
+    }
 
+    private void exit() {
+        mainMenu.stop();
     }
 
     private void showPlayMenu() {
-        playMenu.handleUserInput();
+        playMenu.handleUserSelection();
     }
 
     private void showQuizMenu() {
-        quizMenu.handleUserInput();
+        quizMenu.handleUserSelection();
     }
 
-    public void run() {
-        run = true;
+    public void start() {
         printConsoleHeader();
-
-        // main loop, show the main menu until they exit
-        while (run) {
-            mainMenu.handleUserInput();
-        }
+        mainMenu.handleUserSelection();
+        Console.close();
 
         System.out.println("Thanks for trying out our game jam project for the week1 game jam from Cup of Java discord server!");
         System.out.println("Cup of Java Discord invite: https://discord.gg/yZppYe9Av9");
@@ -62,7 +54,7 @@ public class QuizMaker {
 
     public static void main(String[] args) {
         QuizMaker qm = new QuizMaker();
-        qm.run();
+        qm.start();
 
     }
 
